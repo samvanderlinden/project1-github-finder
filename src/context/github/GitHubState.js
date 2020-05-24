0babfc149d1ff58ec2263f5a1f1a3d695c12ee54
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-05-24 10:56:48 -0500

    configure GitHubState.js to deploy to Netlify

--- src/context/github/GitHubState.js
+++ src/context/github/GitHubState.js
@@ -10,6 +10,19 @@ import {
     GET_REPOS
 } from '../types';
 
+//Initializing GitHub client ID and secret variables
+let gitHubClientId;
+let gitHubClientSecret;
+
+//Checking which environment DEV/PROD we are in
+if(process.env.NODE_ENV !== 'production') {
+    gitHubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    gitHubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    gitHubClientId = process.env.GITHUB_CLIENT_ID;
+    gitHubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GitHubState = props => {
     const initialState = {
         users: [],
@@ -24,7 +37,7 @@ const GitHubState = props => {
     const searchUsers = async text => {
         setLoading();
 
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${gitHubClientId}&client_secret=${gitHubClientSecret}`);
 
         dispatch({
             type: SEARCH_USERS,
@@ -36,7 +49,7 @@ const GitHubState = props => {
     const getUser = async username => {
         setLoading(); //sets 'loading' value to true
 
-        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${gitHubClientId}&client_secret=${gitHubClientSecret}`);
 
         dispatch({
             type: GET_USER,
@@ -49,7 +62,7 @@ const GitHubState = props => {
     const getUserRepos = async username => {
         setLoading();
 
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:ascclient_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:ascclient_id=${gitHubClientId}&client_secret=${gitHubClientSecret}`);
 
         dispatch({
             type: GET_REPOS,
